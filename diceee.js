f95facb8cb2f9915f4360e9222bed80151742c1d
Yash Gupta <yashgupta13.in@gmail.com>
2022-04-01 17:42:11 +0530

    Updated Bugs and fixed dice movement

--- diceee.js
+++ diceee.js
@@ -1,58 +1,86 @@
-var player1 = Math.floor(Math.random()*6)+1;
-var player2 = Math.floor(Math.random()*6)+1;
-
-// alert("It's working");
-
-
-// For player 1;
-
-if(player1==1){
-    document.querySelector(".img1").src = "images/dice1.png";
-}
-if(player1==2){
-    document.querySelector(".img1").src = "images/dice2.png";
-}
-if(player1==3){
-    document.querySelector(".img1").src = "images/dice3.png";
-}
-if(player1==4){
-    document.querySelector(".img1").src = "images/dice4.png";
-}
-if(player1==5){
-    document.querySelector(".img1").src = "images/dice5.png";
-}
-if(player1==6){
-    document.querySelector(".img1").src = "images/dice6.png";
-}
-
-// For player 2;
-
-if(player2==1){
-    document.querySelector(".img2").src = "images/dice1.png";
-}
-if(player2==2){
-    document.querySelector(".img2").src = "images/dice2.png";
-}
-if(player2==3){
-    document.querySelector(".img2").src = "images/dice3.png";
-}
-if(player2==4){
-    document.querySelector(".img2").src = "images/dice4.png";
-}
-if(player2==5){
-    document.querySelector(".img2").src = "images/dice5.png";
-}
-if(player2==6){
-    document.querySelector(".img2").src = "images/dice6.png";
-}
+document.querySelector(".p2").addEventListener("click", play2);
+var player1;
+var player2;
+var count = 0;
 
 
-if(player1>player2){
-    document.querySelector(".result").innerHTML= "Player 1 Wins !!";
+document.querySelector(".p1").addEventListener("click", play1);
+
+function play1() {
+    player1 = Math.floor(Math.random() * 6) + 1;
+
+    // For player 1;
+
+    // alert(player1);
+
+    if (player1 == 1) {
+        document.querySelector(".img1").src = "images/dice1.png";
+    }
+    else if (player1 == 2) {
+        document.querySelector(".img1").src = "images/dice2.png";
+    }
+    else if (player1 == 3) {
+        document.querySelector(".img1").src = "images/dice3.png";
+    }
+    else if (player1 == 4) {
+        document.querySelector(".img1").src = "images/dice4.png";
+    }
+    else if (player1 == 5) {
+        document.querySelector(".img1").src = "images/dice5.png";
+    }
+    else {
+        document.querySelector(".img1").src = "images/dice6.png";
+    }
+    count++;
+    answer();
+
 }
-if(player2>player1){
-    document.querySelector(".result").innerHTML= "Player 2 Wins !!";
+
+function play2() {
+    player2 = Math.floor(Math.random() * 6) + 1;
+
+    // For player 2;
+
+    if (player2 == 1) {
+        document.querySelector(".img2").src = "images/dice1.png";
+    }
+    if (player2 == 2) {
+        document.querySelector(".img2").src = "images/dice2.png";
+    }
+    if (player2 == 3) {
+        document.querySelector(".img2").src = "images/dice3.png";
+    }
+    if (player2 == 4) {
+        document.querySelector(".img2").src = "images/dice4.png";
+    }
+    if (player2 == 5) {
+        document.querySelector(".img2").src = "images/dice5.png";
+    }
+    if (player2 == 6) {
+        document.querySelector(".img2").src = "images/dice6.png";
+    }
+    count++;
+    answer();
 }
-if(player1==player2){
-    document.querySelector(".result").innerHTML= "Oops ! It's a tie";
+
+function answer() {
+
+    if (count % 2 == 0) {
+
+
+        if (player1 > player2) {
+            document.querySelector(".result").innerHTML = "🚩Player 1 Wins !!";
+        }
+        if (player2 > player1) {
+            document.querySelector(".result").innerHTML = "Player 2 Wins !!🚩";
+        }
+        if (player1 == player2) {
+            document.querySelector(".result").innerHTML = "Oops ! It's a tie";
+        }
+    }
+    else {
+        document.querySelector(".result").innerHTML = "Player 2 Chance";
+    }
+
+
 }
